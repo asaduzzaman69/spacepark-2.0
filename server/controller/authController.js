3e2f42ff8e98bd4735f49218bb04222406962aaf
asaduzzaman69 <asaduzzamanhimel12@gmail.com>
2021-07-22 15:49:43 +0600

    created Login endpoint

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,6 +1,9 @@
 const User = require("../model/userModal");
 const catchAsync = require("../utils/catchAsync");
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
+const { AppError } = require("../utils/appError");
+
 
 
 
@@ -34,8 +37,39 @@ const signup = catchAsync( async (req,res) => {
 })
 
 
+const login = catchAsync( async (req,res,next) => {
+
+    const freshUser = await User.find({email: req.body.email})
+
+    if(!Object.keys(freshUser).length) {
+       return next(new AppError("User or password is incorrect", 400))
+    }
+
+
+    const isMatched = await bcrypt.compare(req.body.password, freshUser[0].password)
+
+    if(!isMatched) {
+        return next(new AppError("User or password is incorrect", 400))
+    }
+
+
+    console.log(isMatched)
+
+    const token = jwt.sign({userName: freshUser.userName, _id: freshUser._id}, process.env.JWT_PRIVATE_KEY, {
+        expiresIn: '1d'
+    })
+
+    res.status(200).json({
+        status: 'success',
+        token
+    })
+
+}) 
+
+
 
 
 module.exports = {
-    signup
+    signup,
+    login
 }
--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { signup } = require("../controller/authController");
+const { signup,login } = require("../controller/authController");
 const { getAllUsers } = require("../controller/userController");
 
 const userRouter = express.Router();
@@ -8,8 +8,8 @@ const userRouter = express.Router();
 // Auth Route
 
 userRouter.post('/signup', signup)
-/* router.post('/signup', login)
- */userRouter.route("/").get(getAllUsers);
-
+userRouter.post('/login', login)
+/*  userRouter.route("/").get(getAllUsers);
+ */
 
  module.exports = userRouter;
